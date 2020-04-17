262a866fe3af9d596b6e7bcf1860508ca5118e44
Liam Reardon <lmr647@mun.ca>
2020-04-17 16:20:27 -0230

    setup ios project

---
+++ windmill-ios/windmill/windmill/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  windmill
+//
+//  Created by Liam  on 2020-04-17.
+//  Copyright © 2020 Liam Reardon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("windmill: the looping video app")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
